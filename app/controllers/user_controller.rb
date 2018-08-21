15bc68d0573e32857f72aeffd0d1e405ac3f1285
lifeprolific <thegregoman@gmail.com>
2018-08-21 15:49:40 -0400

    added starred, followers, following apis

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,5 +7,8 @@ class UserController < ApplicationController
     @user_api = HTTParty.get('https://api.github.com/users/' + user_name, :basic_auth => auth)
     @repos_api = HTTParty.get('https://api.github.com/users/' + user_name + '/repos', :basic_auth => auth)
     @orgs_api = HTTParty.get('https://api.github.com/users/' + user_name + '/orgs', :basic_auth => auth)
+    @starred_api = HTTParty.get('https://api.github.com/users/' + user_name + '/starred', :basic_auth => auth)
+    @followers_api = HTTParty.get('https://api.github.com/users/' + user_name + '/followers', :basic_auth => auth)
+    @following_api = HTTParty.get('https://api.github.com/users/' + user_name + '/following', :basic_auth => auth)
   end
 end

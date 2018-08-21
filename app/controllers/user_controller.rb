1d4d2f18f38c69e6c0e9ce9240ed89271d94960d
lifeprolific <thegregoman@gmail.com>
2018-08-21 14:11:56 -0400

    Added title using API

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,8 @@
+require 'httparty'
+
 class UserController < ApplicationController
   def index
+    auth = {:username => "lifeprolific", :password => "TOKEN"}
+    @user_response = HTTParty.get('https://api.github.com/users/sarahmei', :basic_auth => auth)
   end
 end

116f60f5b80012e2eece9c92a8355a7f9bdf4968
lifeprolific <thegregoman@gmail.com>
2018-08-21 15:30:00 -0400

    Added left hand profile html

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,10 @@ require 'httparty'
 
 class UserController < ApplicationController
   def index
+    user_name = 'sarahmei'
     auth = {:username => "lifeprolific", :password => "TOKEN"}
-    @user_response = HTTParty.get('https://api.github.com/users/sarahmei', :basic_auth => auth)
+    @user_api = HTTParty.get('https://api.github.com/users/' + user_name, :basic_auth => auth)
+    @repos_api = HTTParty.get('https://api.github.com/users/' + user_name + '/repos', :basic_auth => auth)
+    @orgs_api = HTTParty.get('https://api.github.com/users/' + user_name + '/orgs', :basic_auth => auth)
   end
 end
